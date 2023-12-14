d3ad1791eac07697b4ae6c4b2a1f16f59fdde512
ekoi <eko.indarto@dans.knaw.nl>
2023-12-14 09:05:14 +0100

    Initial version

---
+++ src/StarRating.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { Rating } from 'react-simple-star-rating'
+
+const tooltipArray = [
+  "Terrible",
+  "Bad",
+  "Average",
+  "Great",
+  "Awesome",
+];
+interface RatingProps {
+  user_id: string
+  page_id: string
+  rate: number
+  readonly : boolean
+  handleRating: any
+}
+export default function StarRating({ user_id, page_id, rate , readonly, handleRating}: RatingProps) {
+  return (
+    <div>
+      <Rating
+        onClick={(number) => handleRating(number)}
+        initialValue={rate}
+        readonly={readonly}
+        showTooltip
+        tooltipArray={tooltipArray}
+      />
+    </div>
+  )
+}
