7e512494d8be9e87388804985bf1fecbe82a819d
Laurens Tobias <akeldama.yggdrasill@gmail.com>
2024-05-14 15:42:52 +0200

    Removed old codebase

--- dist/cjs/types/StarRating.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-interface RatingProps {
-    rate: number;
-    readonly: boolean;
-    handleRating: (stars: number) => void;
-}
-export default function StarRating({ rate, readonly, handleRating }: RatingProps): React.JSX.Element;
-export {};
